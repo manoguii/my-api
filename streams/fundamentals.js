9bd1679f0001357e89c1aac4b1466cd7820c122a
Guilherme David <guilhermedavidrk@gmail.com>
2023-01-11 00:49:10 -0300

    feat: streams and node

---
+++ streams/fundamentals.js
@@ -0,0 +1,45 @@
+import { Writable, Transform } from "stream"
+
+class OneToHoundredStream extends Readable {
+  index = 0
+  
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i >= 10) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+  
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transform = Number(chunk.toString()) * -1
+
+    console.log(transform)
+
+    callback(null, Buffer.from(String(transform)))
+  }
+}
+
+class MultiplyByTeanStream extends Writable {
+  array = []
+
+  _write(chunk, encoding, callback) {
+    this.array.push(Number(chunk.toString()) * 10)
+    
+    console.log(this.array)
+
+    callback()
+  }
+}
+
+new OneToHoundredStream()
+  .pipe(new InverseNumberStream)
+  .pipe(new MultiplyByTeanStream())
